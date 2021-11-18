100ce91e51f3d299a2cf0debc148f4cd287c05da
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-18 11:31:14 +0100

    rectified error

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-const trashContainer = document.querySelector(".trash-contanier")
-
+const trashContainer = document.querySelector(".trash-container")
 const moneyElem = document.querySelector(".money")
-
-const curencyFormatter = new Intl.NumberFormat("en-us", {
-    style: "currency",
-    currency: "GBP",
-    maximumFractionDigits: 0
+const currencyFormatter = new Intl.NumberFormat("en-us", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
 })
-
 const trashFormatter = new Intl.NumberFormat("en-us", {
-    minimumIntegerDigits: 8,
-    maximumFractionDigits: 0,
-    useGrouping: false
+  minimumIntegerDigits: 8,
+  maximumFractionDigits: 0,
+  useGrouping: false,
 })
 
 const MAX_MONEY_RAISED = 30000000
-
 
 setupTrash()
 
@@ -26,55 +22,55 @@ async function setupTrash() {
   moneyElem.innerText = currencyFormatter.format(amountRaised)
 
   const amountLeftToRaise = Math.max(MAX_MONEY_RAISED - amountRaised, 0)
-  const stringifiedAmount = (trashFormatter.format(amountLeftToRaise))
+  const stringifiedAmount = trashFormatter.format(amountLeftToRaise)
   const trashAmount = {
-      xxl: {
-          amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
-          icon: "bag",
-      },
-      xl: {
-        amount: parseInt(stringifiedAmount[2]),
-        icon: "takeout",
-      },
-      lg: {
-        amount: parseInt(stringifiedAmount[3]),
-        icon: "headphones",
-      },
-     md: {
-        amount: parseInt(stringifiedAmount[3]),
-        icon: "phone",
-      },
-     sm: {
-        amount: parseInt(stringifiedAmount[4]),
-        icon: "toy-car",
-      },
-     xs: {
-        amount: parseInt(stringifiedAmount[5]),
-        icon: "bottle",
-      },
+    xxl: {
+      amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
+      icon: "bag",
+    },
+    xl: {
+      amount: parseInt(stringifiedAmount[2]),
+      icon: "takeout",
+    },
+    lg: {
+      amount: parseInt(stringifiedAmount[3]),
+      icon: "headphones",
+    },
+    md: {
+      amount: parseInt(stringifiedAmount[4]),
+      icon: "phone",
+    },
+    sm: {
+      amount: parseInt(stringifiedAmount[5]),
+      icon: "toy-car",
+    },
+    xs: {
+      amount: parseInt(stringifiedAmount[6]),
+      icon: "bottle",
+    },
+  }
+
+  Object.values(trashAmount).forEach(({ amount, icon }) => {
+    for (let i = 0; i < amount; i++) {
+      createTrash(icon)
     }
-  Object.values(trashAmount).forEach(({amount. icon}) => {
-      for (let i = 0; i < amount; i++) {
-          createTrash(icon)
-      }
   })
-  
 }
+
 function createTrash(icon) {
-    const img = document.createElement("img")
-    const top = randomNumberBetween(0, 50)
-    const size = top / 5 + 1 
-    img.classList.add("trash")
-    img.src = '/imgs/${icon).svg'
-    img.style,width = `${size}vmin`
-    img.style,height = `${size}vmin`
-    img.style.top = `${top}vh`
-    img.style.left = `${randomNumberBetween(0, 100)}vh`
-    img.style.setProperty("--rotation", `${randomNumberBetween(-30, -30)}deg`)
-    trashContainer.appendChild(img)
+  const img = document.createElement("img")
+  const top = randomNumberBetween(0, 50)
+  const size = top / 5 + 1
+  img.classList.add("trash")
+  img.src = `/imgs/${icon}.svg`
+  img.style.width = `${size}vmin`
+  img.style.height = `${size}vmin`
+  img.style.top = `${top}vh`
+  img.style.left = `${randomNumberBetween(0, 100)}vw`
+  img.style.setProperty("--rotation", `${randomNumberBetween(-30, 30)}deg`)
+  trashContainer.appendChild(img)
 }
 
 function randomNumberBetween(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
